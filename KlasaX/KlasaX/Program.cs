19038cbb702bfc411d315b37338d46b09e8220b9
Raul-Holik <113007938+Raul-Holik@users.noreply.github.com>
2022-12-06 18:12:46 +0100

    Add files via upload

---
+++ KlasaX/KlasaX/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlasaX
+{
+    static class KlasaX
+    {
+        static private int broj;
+
+        static public int Broj { get => broj; set => broj = value; }
+    }
+        internal class Program
+    {
+        static void Main(string[] args)
+        {
+            KlasaX.Broj = 10;
+            Console.WriteLine("Vrijednost varijable je : "+Convert.ToString(KlasaX.Broj)+".");
+
+            Console.ReadKey();
+        }
+    }
+}
